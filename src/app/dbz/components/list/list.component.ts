b867890f5a156f287fb08c7189c01dabf24de431
eduard.sanchez <eduard.sanchez@sofka.com.co>
2023-08-28 15:40:27 -0500

    services section

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character-interface';
 
 @Component({
@@ -8,9 +8,16 @@ import { Character } from '../../interfaces/character-interface';
 })
 export class ListComponent {
 
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter();
+
   @Input()
   public characterList: Character[] = [{
     name: 'Trunks',
     power: 10
   }];
+
+  onDeleteCharacter(index: number):void {
+    this.onDelete.emit(index);
+  }
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Character } from '../interfaces/character-interface';
+import { DbzService } from '../services/dbz.service';
 
 
 @Component({
@@ -7,17 +7,5 @@ import { Character } from '../interfaces/character-interface';
   templateUrl: 'main-page.component.html'
 })
 export class MainPageComponent {
-  public characters: Character[] = [{
-      name: 'Killin',
-      power: 1000
-    },
-    {
-      name: 'Goku',
-      power: 9500
-    },
-    {
-      name: 'Vegeta',
-      power: 7500
-    }
-  ];
+  constructor( public dbzService: DbzService ) {}
 }
